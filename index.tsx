30efc38966894f55a01f19d274a7795c179b629e
Akshay Ranjan <akshay.ranjan@gmail.com>
2024-02-15 17:30:08 +0530

    Fixed dependency issues.

--- index.tsx
+++ index.tsx
@@ -1,8 +1,10 @@
 import Form from '@rjsf/chakra-ui';
+import { FormProps } from '@rjsf/core';
 import validator from '@rjsf/validator-ajv8';
+import React from 'react';
 
-const FormWrapper = (props) => {
-    return <Form {...props} validator={validator} />;
+const FormWrapper = (props: React.JSX.IntrinsicAttributes & FormProps<any, any, any>) => {
+    return (<Form {...props} validator={validator} />);
 };
 
 export default FormWrapper;
