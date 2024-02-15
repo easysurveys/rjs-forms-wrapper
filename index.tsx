5424c61160ba6459cfe3af9030395e23c1ff87cf
Akshay Ranjan <akshay.ranjan@gmail.com>
2024-02-15 17:32:59 +0530

    More updates.

--- index.tsx
+++ index.tsx
@@ -1,9 +1,8 @@
 import Form from '@rjsf/chakra-ui';
-import { FormProps } from '@rjsf/core';
 import validator from '@rjsf/validator-ajv8';
 import React from 'react';
 
-const FormWrapper = (props: React.JSX.IntrinsicAttributes & FormProps<any, any, any>) => {
+const FormWrapper = (props) => {
     return (<Form {...props} validator={validator} />);
 };
 
